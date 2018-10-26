fc3681d4be2621385d0638a1c78380c1d6e4092c
Claire Goeckner-Wald <cgoecknerwald@gmail.com>
2018-10-26 17:01:40 +0200

    Add basic slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,5 +29,15 @@
 			Tone.Transport.start();
 		  }
 	});
+
+	// Slider
+	var slider = document.getElementById("slider");
+	var output = document.getElementById("slider-value");
+	output.innerHTML = slider.value;
+
+	// Update the current slider value (each time you drag the slider handle)
+	slider.oninput = function() {
+	    output.innerHTML = this.value;
+	} 
 	
 })();
