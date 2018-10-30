93837cfa6f0c98abb626d30d79adef7415c89f97
xiongamy <xiong.yamy@gmail.com>
2018-10-30 19:14:43 +0100

    Allow varied note lengths
    The one-measure rhythms can now have different (non-dotted, non-triplet) note lengths.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,52 @@
 
 	// Strict mode changes previously accepted "bad syntax" into real errors.
 	"use strict";
-
+	
+	// Note: Everything assumes 4/4 time
 	var Tone = window.Tone;
 	var synth = new Tone.Synth();
 	synth.toMaster();
 	
-	var pitches = ["C4", "E4", "G4", "A4"];
-	var rhythms = [["0", "0:2", "0:3:2"], ["0", "0:0:2", "0:1", "0:2", "0:3"]];
+	Tone.Transport.bpm.value = 80;
+	
+	// Converts input note length from a float (1 = quarter, 0.5 = eighth, etc.)
+	// to Tone's string format ("4n", "8n", etc.).
+	// For now, can only handle non-dotted and non-triplet notes.
+	function getNoteLength(length) {
+		var exp = Math.log2(length);
+		if (exp % 1 == 0 && length < 4) {
+			return Math.pow(2, -exp + 2) + "n";
+		} else if (length % 4 == 0) {
+			return Math.floor(length / 4) + "m";
+		} else {
+			throw new Error("invalid note length");
+		}
+	}
+	
+	// Converts input time from a float (0 as start of measure, 1 as first quarter)
+	// to Tone's bars:quarters:sixteenths format.
+	function getNoteTime(time) {
+		var bars = Math.floor(time / 4);
+		var quarters = Math.floor(time - bars);
+		var sixteenths = (time - bars - quarters) * 4;
+		return bars + ":" + quarters + ":" + sixteenths;
+	}
+	
+	var pitches = ["C4", "E4", "", "G4", "A4"];
+	var rhythms = [[2, 1, 0.5, 0.5], [0.5, 0.5, 1, 1, 1]];
 	
 	function createMeasure(rhythm) {
 		var notes = [];
+		var totalTime = 0;
 		for (var i = 0; i < rhythm.length; i++) {
-			notes.push([rhythm[i] , pitches[i % pitches.length]]);
+			var pitch = pitches[i % pitches.length];
+			var length = getNoteLength(rhythm[i]);
+			var time = getNoteTime(totalTime);
+			totalTime += rhythm[i];
+			notes.push({"time" : time, "pitch" : pitch, "length" : length});
 		}
-		new Tone.Part(function(time, note) {
-			synth.triggerAttackRelease(note, "8n", time);
+		new Tone.Part(function(time, value) {
+			synth.triggerAttackRelease(value.pitch, value.length, time);
 		}, notes).start("+0");
 	}
 
