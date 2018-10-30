c08494fea76d79b82eed748c498abd09039b58da
xiongamy <xiong.yamy@gmail.com>
2018-10-30 23:33:52 +0100

    Randomize pitches
    Randomly choose pitches from the C major scale. Pitches change in small intervals from note to note.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,19 @@
 	// Strict mode changes previously accepted "bad syntax" into real errors.
 	"use strict";
 	
+	// Choose a random entry from the array
+	function pickRandom(arr) {
+		return arr[Math.floor(Math.random() * arr.length)];
+	}
+	
 	// Note: Everything assumes 4/4 time
-	var Tone = window.Tone;
+	const Tone = window.Tone;
 	var leadLine = new Tone.Synth();
 	leadLine.toMaster();
 	var bassLine = new Tone.Synth();
 	bassLine.toMaster();
 	
-	Tone.Transport.bpm.value = 80;
+	Tone.Transport.bpm.value = 100;
 	
 	// Converts input note length from a float (1 = quarter, 0.5 = eighth, etc.)
 	// to Tone's string format ("4n", "8n", etc.).
@@ -35,31 +40,61 @@
 		return bars + ":" + quarters + ":" + sixteenths;
 	}
 	
-	// Note patterns and rhythm patterns to choose from
-	var pitches = ["C4", "E4", "G4", "A4"];
-	var rhythms =  [[4], [2, 2], [1, 1, 1, 1],
+	// Pitch, intervals, and rhythm patterns to choose from
+	const pitches = ["C", "D", "E", "F", "G", "A", "B"];
+	const intervals = [0, 0, 1, 1, 1, 2, 2, 3];
+	const rhythms =  [[4], [2, 2], [1, 1, 1, 1],
 					[1, 1, 2], [1, 2, 1], [2, 1, 1],
 					[1, 3], [3, 1]];
+	const minOctave = 2, maxOctave = 5;
+					
+	var currPitchIndex = 0, currOctave = 4;
 	
 	// Create a full, half, or quarter measure of randomly-generated music.
 	// Generate up to maxLength (in number of quarter notes) of music,
 	// with given offset from the start of the measure.
 	function createMeasure(maxLength, offset) {
-		var rhythm = rhythms[Math.floor(Math.random() * rhythms.length)];
+		var rhythm = pickRandom(rhythms);
 		var notes = [];
 		var totalTime = offset;
 		
 		// create a full, half, or quarter measure
 		var sectionLength = Math.pow(2, Math.floor(Math.random() * Math.log2(maxLength)));
+		console.log(maxLength + " " + offset + " " + sectionLength);
 		var multiplier = sectionLength / 4;
 		
 		// determine note length, timing, & pitch, for each note in the rhythm
 		for (var i = 0; i < rhythm.length; i++) {
-			var pitch = pitches[i % pitches.length];
+			// randomize pitch
+			var pitchIndex, pitch, octave;
+			do {
+				var interval = pickRandom(intervals);
+				if (Math.random() > 0.5) {
+					interval *= -1;
+				}
+				pitchIndex = currPitchIndex + interval;
+				octave = currOctave;
+				if (pitchIndex < 0) {
+					pitchIndex += pitches.length;
+					octave--;
+				} else if (pitchIndex >= pitches.length) {
+					pitchIndex -= pitches.length;
+					octave++;
+				}
+				pitch = pitches[pitchIndex] + octave;
+			} while (octave < minOctave || octave > maxOctave);
+			currPitchIndex = pitchIndex;
+			currOctave = octave;
+			
+			// determine note length and timing
 			var length = getNoteLength(rhythm[i] * multiplier)
 			var time = getNoteTime(totalTime);
 			totalTime += rhythm[i] * multiplier;
-			notes.push({"time" : time, "pitch" : pitch, "length" : length});
+			
+			if (pitch != "") {
+				notes.push({"time" : time, "pitch" : pitch, "length" : length});
+			}
+			// console.log(time + " " + pitch + " " + length);
 		}
 		
 		new Tone.Part(function(time, value) {
@@ -78,7 +113,7 @@
 	}, "1m").start(0);
 	
 	var baseLoop = new Tone.Loop(function() {
-		bassLine.triggerAttackRelease("A3", "8n", "+0", 1);
+		bassLine.triggerAttackRelease("G3", "8n", "+0", 1);
 		bassLine.triggerAttackRelease("G3", "8n", "+4n", 0.5);
 		bassLine.triggerAttackRelease("G3", "8n", "+2n", 0.5);
 		bassLine.triggerAttackRelease("G3", "8n", "+2n.", 0.5);
